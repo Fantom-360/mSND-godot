a91b0a6994aa2ccd18b29ab39367fe4e2d9c7c18
Fantom-360 <sammlynek7@gmail.com>
2026-02-09 08:58:38 +0100

    update

--- UI_control_scenes/UI_scripts/campain_menu.gd
+++ UI_control_scenes/UI_scripts/campain_menu.gd
@@ -6,7 +6,7 @@ func _on_return_button_button_up() -> void:
 
 
 func _on_level_1_button_up() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://leves/level1/level_1.tscn")
 
 
 func _on_level_2_button_up() -> void:
---
+++ leves/level1/game_logic.gd
@@ -0,0 +1,114 @@
+extends Node
+#game logic script
+
+var grid := {}  #{ Vector2i(q, r): true}
+var units := [] # list of unit nodes
+const MAP_PATH := "user://maps/level1.json"
+@onready var hex_overlay := get_parent().get_node("HexOverlay")
+var selected_unit = null
+const NEIGHBORS_EVEN = [
+	Vector2i(+1, 0), Vector2i(+1, -1),
+	Vector2i(0, -1), Vector2i(-1, -1),
+	Vector2i(-1, 0), Vector2i(0, +1)
+]
+
+const NEIGHBORS_ODD = [
+	Vector2i(+1, +1), Vector2i(+1, 0),
+	Vector2i(0, -1), Vector2i(-1, 0),
+	Vector2i(-1, +1), Vector2i(0, +1)
+]
+
+
+func _ready() -> void:
+	generate_grid()
+	#if map_file_exists():
+		#load_map()
+	#else:
+		#generate_grid()
+		#save_map()
+
+func get_neighbors(h: Vector2i) -> Array:
+	var dirs = NEIGHBORS_ODD if h.x % 2 == 1 else NEIGHBORS_EVEN
+	var result := []
+
+	for d in dirs:
+		var n = h + d
+		if grid.has(n):
+			result.append(n)
+
+	return result
+
+func generate_grid():
+	for q in range(15):
+		for r in range(7):
+			grid[Vector2i(q, r)] = {
+				"q": q,
+				"r": r,
+				"walkable": true,
+				"unit": null
+			}
+
+func register_unit(unit):
+	units.append(unit)
+	grid[unit.hex]["unit"] = unit
+	
+func spawn_unit(hex: Vector2i):
+	var unit = preload("res://leves/level1/units/unit.tscn").instantiate()
+	unit.hex = hex
+	get_parent().get_node("Units").add_child(unit)
+	register_unit(unit)
+	
+func load_map():
+	grid.clear()
+
+	var file = FileAccess.open(MAP_PATH, FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	file.close()
+
+	for hex_data in data["hexes"]:
+		var h = Vector2i(hex_data["q"], hex_data["r"])
+		grid[h] = {
+			"q": h.x,
+			"r": h.y,
+			"walkable": hex_data["walkable"],
+			"unit": null
+		}
+
+func save_map():
+	var data := {}
+	data["hexes"] = []
+	
+	for h in grid.keys():
+		data["hexes"].append({
+			"q": h.x,
+			"r": h.y,
+			"walkable": grid[h]["walkable"]
+		})
+	DirAccess.make_dir_recursive_absolute("user://maps")
+	
+	var file = FileAccess.open(MAP_PATH, FileAccess.WRITE)
+	file.store_string(JSON.stringify(data))
+	file.close()
+	
+func map_file_exists():
+	return FileAccess.file_exists(MAP_PATH)
+	
+func update_unit_pos():
+	for unit in units:
+		unit.move_to_hex(unit.hex)
+		
+func select_unit(unit):
+	selected_unit = unit
+	hex_overlay.reachable_hexes = get_neighbors(unit.hex)
+	hex_overlay.queue_redraw()
+
+func clear_selection():
+	selected_unit = null
+	hex_overlay.reachable_hexes.clear()
+	hex_overlay.queue_redraw()
+	
+func on_hex_clicked(hex: Vector2i):
+	if grid[hex]["unit"] != null:
+		select_unit(grid[hex]["unit"])
+	else:
+		clear_selection()
